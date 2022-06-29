0d9025cb34ef22da47a4328aa9617fcbf7ebc92a
jelenapetrovacki <jpetrovacki3@gmail.com>
2022-06-29 17:21:49 +0200

    Authorization SpringBoot project created

---
+++ create-auth-project.bash
@@ -0,0 +1,14 @@
+cd spring-cloud
+
+spring init \
+--boot-version=2.3.2.RELEASE \
+--build=gradle \
+--java-version=1.8 \
+--packaging=jar \
+--name=authorization-server \
+--package-name=se.magnus.springcloud \
+--groupId=se.magnus.springcloud \
+--version=1.0.0-SNAPSHOT \
+authorization-server
+
+cd ..
